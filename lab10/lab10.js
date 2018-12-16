6e6502a116618709924f0f171dfb3238d5b6224f
Aaron Goldbin <31373437+agoldbin@users.noreply.github.com>
2018-12-15 19:16:32 -0600

    receiving urls from search. Just need to display gifs to page

--- lab10/lab10.js
+++ lab10/lab10.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-    // alert("test")
-    // $("li").
+    // Select all li tags (current classes)
+    const classes = [...document.querySelectorAll("li")];
+
+    // TODO see if this is even close to right...
+    const myClasses = ['Mobile App Internship', 'iOS Development'];
+    // let allClasses = [...classes, myClasses];
+
+    // map all current classes
+    classes.map(currentClass => {
+        // allClasses.textContent;
+        console.log(currentClass.textContent);
+        console.log(...myClasses);
+    });
+
+    // add 2 more classes to map
+
+    // print array to console
 })
--- project4/project4.js
+++ project4/project4.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
     // When search button is clicked, the magic begins...
     $("#searchBtn").click(function() {
-        // const xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${$('#gifSearchField').val()}&api_key=0LiVOxtcLaAecDKwpJAS70WorYkySwZB&limit=10`);
-        // xhr.done(function(data) {
+        // variables which will be reused for appending to the ul on index
+        const newItem = $("<li></li>");
+        const gifImg  = $("<img>");
+        newItem.append(gifImg);
 
         // perform HTTP get request and process response
-        $.get(`http://api.giphy.com/v1/gifs/search?q=${$('#gifSearchField').val()}&api_key=0LiVOxtcLaAecDKwpJAS70WorYkySwZB&limit=10`, function( data ) {
-            // $( ".result" ).html( data );
-            console.log(data);
+        // Split onto multiple lines for readability
+        $.get(`http://api.giphy.com/v1/gifs/search?q=
+                ${$('#gifSearchField').val()}
+                &api_key=0LiVOxtcLaAecDKwpJAS70WorYkySwZB&limit=10`
+                , function( data ) {
+        }).done(function(data) {
+            // Save response of returned gif data, everything else is junk
+            const gifData = data.data;
+            // console.log(data);
 
+            // This line returns an array with the urls to the gifs to be displayed
+            // const gifURLs = gifData.map(gifData => gifData.images.downsized.url);
 
-            // console.log(JSON.parse(data));
-            // console.log("success got data", data);
-
-            // Parse data to JSON
-            // const gifJSON = JSON.parse(data);
-
-            // iterate over JSON using .map
-            const gifArray = [];
-
-            // console.log(JSON.parse(data));
-
+            // TODO display each gif and append to the page
+            const gifURLs = gifData.map(gifData => {
+                let currentGif = {};
+                currentGif.url = gifData.images.downsized.url;
+                return currentGif;
+                // $("#gifResults").append(newItem.append(gifImg.attr("src", gifData.images.downsized.url)));
+            })
+            console.table(gifURLs);
         });
     })
 })
